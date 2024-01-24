942fe2060c3643242ca03bdb145072e04fd42dad
Changmin Kim <ckd29672@naver.com>
2024-01-24 14:19:32 +0900

    initialize project

---
+++ src/main/java/com/wesp/wespserver/WespserverApplication.java
@@ -0,0 +1,13 @@
+package com.wesp.wespserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WespserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WespserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wesp/wespserver/WespserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesp.wespserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WespserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
